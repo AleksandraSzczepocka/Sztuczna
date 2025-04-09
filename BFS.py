a5b4fd421447b028b93560afa39c6f188de7666c
251642 Szczepocka Aleksandra <251642@edu.p.lodz.pl>
2025-04-09 15:49:57 +0200

    BFS

---
+++ BFS.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+from Sztuczna import PuzzleState
+
+
+def bfs(start: PuzzleState)-> str:
+    if start.is_goal():
+        return start.path
+
+    visited = set()
+    queue = deque()
+    queue.append(start)
+    visited.add(start)
+
+    while queue:
+        current = queue.popleft()
+
+        for neighbor in current.neighbors:
+            if neighbor not in visited:
+                if neighbor.is_goal():
+                    return neighbor.path
+                queue.append(neighbor)
+                visited.add(neighbor)
+
+    return "Fail" # można poprawić to
--- PuzzleState.py
+++ PuzzleState.py
@@ -24,3 +24,23 @@ class PuzzleState:
     def __eq__(self, other):
         return self.board == other.board
 
+    def get_neighbours(self) -> List['PuzzleState']:
+        neighbours = []
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        x, y = self.zero_pos
+
+        for direction in directions:
+            mx, my = directions[direction]
+            new_x, new_y = x + mx, y + my
+
+            if 0 <= new_x < len(self.board) and 0 <= new_y < len(self.board[0]):
+                new_board = [row[:] for row in self.board]
+
+                new_board[x][y], new_board[new_x][new_y] = new_board[new_x][new_y], new_board[x][y]
+
+                neighbours.append(PuzzleState(new_board, (new_x, new_y), self.path + direction))
+
+            return neighbours
+
+
