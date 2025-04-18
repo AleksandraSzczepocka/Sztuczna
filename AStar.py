2cb29b9dd51c340f30d597454589b94688d5e0a0
251604 Pakos Dawid <251604@edu.p.lodz.pl>
2025-04-18 19:21:26 +0200

    AStar not working

--- AStar.py
+++ AStar.py
@@ -1,2 +1,42 @@
 import time
+import heapq
+from typing import Any
 
+from PuzzleState import PuzzleState
+
+
+def astar(start: PuzzleState, parameter: str) -> tuple[str, int, int, int, float] | str | Any:
+    start_time = time.perf_counter()
+
+    if start.is_goal():
+        return start.path, 1, 0, 0, time.perf_counter() - start_time
+
+    priority_queue = []
+    heapq.heappush(priority_queue, (start.cost + start.heuristic(parameter), start))
+
+    visited = set()
+    visited_count = 0
+    processed_count = 0
+    max_depth = 0
+
+    while priority_queue:
+        _, current = heapq.heappop(priority_queue)
+
+        if current.is_goal():
+            return current.path, visited_count + 1, processed_count, max_depth, time.perf_counter() - start_time
+
+        if current not in visited:
+            visited.add(current)
+            visited_count += 1
+            max_depth = max(max_depth, len(current.path))
+
+            neighbors = current.get_neighbours(parameter)
+
+            for neighbor in neighbors:
+                if neighbor not in visited:
+                    f = neighbor.cost + neighbor.heuristic(parameter)
+                    heapq.heappush(priority_queue, (f, neighbor))
+
+            processed_count += 1
+
+    return "Fail", visited_count, processed_count, max_depth, time.perf_counter() - start_time
